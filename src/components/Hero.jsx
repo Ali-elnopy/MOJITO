8fb8bd754da3d078e65c2c33c01805f72a8b605b
Ali <utfcystgvytf@gmail.com>
2025-12-26 11:21:33 +0200

    add Hero Section part-2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
 import gsap from 'gsap'
-import React from 'react'
+import React, { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive';
 
 function Hero() {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
     useGSAP(()=>{
         const heroSplit = new SplitText('.title',{type: 'chars, words',});
         const paragraphSplit  = new SplitText('.subtitle',{type: 'lines',});
@@ -33,8 +36,26 @@ function Hero() {
                 scrub: true,
             }
         })
-        .to('.right-leaf', {y:200},0)
-        .to('.left-leaf', {y:-200},0)
+        .to('.right-leaf', {y:200} , 0)
+        .to('.left-leaf' , {y:-200}, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue  = isMobile ? '120% top' :  'bottom top';
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+               trigger: "video",
+               start: startValue,
+               end: endValue,
+               scrub: true,
+               pin: true,
+            },
+           });
+        videoRef.current.onloadedmetadata = ()=>{
+            tl.to(videoRef.current,{
+                currentTime: videoRef.current.duration,
+            });
+        };
     },[])
   return (
     <>
@@ -62,6 +83,16 @@ function Hero() {
             </div>
 
         </section>
+        <div className="video absolute inset-0">
+            <video 
+                ref={videoRef}
+                src="/videos/input.mp4"
+                muted
+                playsInline
+                preload='auto'
+            />
+        </div>
+    
     </>
   )
 }
