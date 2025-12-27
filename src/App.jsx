5c80ae9459b1506015c7c0a167cccecf9215277c
Ali <utfcystgvytf@gmail.com>
2025-12-27 12:38:46 +0200

    fix import path for NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from 'gsap'
 import {ScrollTrigger, SplitText} from 'gsap/all'
 import React from 'react'
-import NavBar from './components/navBar'
+import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
 import About from './components/About'
